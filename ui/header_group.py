9b0c6c89e3b424eafe4fa1dac88f3fb0a7b304f6
ChoiWonJae850610 <choiwonjae@kakao.com>
2026-03-01 23:01:37 +0900

    UI 제목 부분 수정
    UI 살짝 수정

--- ui/header_group.py
+++ ui/header_group.py
@@ -4,7 +4,7 @@ from PySide6.QtCore import QDate
 
 class HeaderGroup(QGroupBox):
     def __init__(self):
-        super().__init__("상단 정보")
+        super().__init__("제품 정보")
 
         layout = QFormLayout(self)
         layout.setVerticalSpacing(6)
@@ -24,10 +24,10 @@ class HeaderGroup(QGroupBox):
         for w in (self.style_no, self.factory, self.store):
             w.setFixedHeight(26)
 
-        layout.addRow("날짜", self.date)
-        layout.addRow("style no", self.style_no)
-        layout.addRow("공장", self.factory)
-        layout.addRow("매장", self.store)
+        layout.addRow("날   짜", self.date)
+        layout.addRow("제품명", self.style_no)
+        layout.addRow("공   장", self.factory)
+        layout.addRow("매   장", self.store)
 
         self.setSizePolicy(QSizePolicy.Preferred, QSizePolicy.Fixed)
 
