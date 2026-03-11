dcedd3fcf7f1e07507c1a3c7174e58826c6e1038
ChoiWonJae850610 <choiwonjae@kakao.com>
2026-03-11 15:48:53 +0900

    작업지시서 UI 배치 수정
    작업지시서 UI 배치 수정

--- ui/work_order_page.py
+++ ui/work_order_page.py
@@ -1,3 +1,4 @@
+
 from __future__ import annotations
 
 from dataclasses import dataclass
@@ -7,7 +8,7 @@ from PySide6.QtWidgets import QGridLayout, QHBoxLayout, QLabel, QPushButton, QSt
 
 from ui.icon_factory import make_image_outline_icon, make_save_icon, standard_icon
 from ui.image_preview import ImagePreview
-from ui.postit_widgets import ChangeNotePostIt, FolderTabHeader, PostItBar, SectionContainer
+from ui.postit_widgets import ChangeNotePostIt, PostItBar, SectionContainer, SectionTitleBadge
 from ui.theme import THEME, image_preview_style
 from ui.widget_factory import apply_icon_button_metrics
 
@@ -31,111 +32,87 @@ class WorkOrderPageBuilder:
     @staticmethod
     def build(parent: QWidget) -> WorkOrderPageRefs:
         page = QWidget()
+
         page_layout = QVBoxLayout(page)
-        page_layout.setContentsMargins(THEME.page_padding_x, THEME.page_padding_y, THEME.page_padding_x, THEME.page_padding_y)
+        page_layout.setContentsMargins(
+            THEME.page_padding_x,
+            THEME.page_padding_y,
+            THEME.page_padding_x,
+            THEME.page_padding_y,
+        )
         page_layout.setSpacing(THEME.block_spacing)
 
-        btn_back = QPushButton('◀')
-        apply_icon_button_metrics(btn_back, font_px=THEME.icon_button_font_px + 2, object_name='navButton', tooltip='뒤로가기')
+        btn_back = QPushButton("◀")
+        apply_icon_button_metrics(btn_back, font_px=THEME.icon_button_font_px + 2, object_name="navButton", tooltip="뒤로가기")
 
-        btn_reset = QPushButton('')
-        apply_icon_button_metrics(btn_reset, font_px=THEME.reset_button_font_px, object_name='iconAction', tooltip='새로고침')
-        btn_reset.setIcon(standard_icon(parent, [QStyle.SP_BrowserReload, QStyle.SP_FileDialogDetailedView]))
-        btn_reset.setIconSize(QSize(THEME.icon_size_md, THEME.icon_size_md))
+        btn_reset = QPushButton("")
+        apply_icon_button_metrics(btn_reset, font_px=THEME.reset_button_font_px, object_name="iconAction", tooltip="새로고침")
+        btn_reset.setIcon(standard_icon(parent, [QStyle.SP_BrowserReload]))
 
-        btn_save = QPushButton('')
-        apply_icon_button_metrics(btn_save, font_px=THEME.save_button_font_px, object_name='iconPrimary', tooltip='저장')
-        save_icon = standard_icon(parent, [QStyle.SP_DialogSaveButton], fallback=make_save_icon(THEME.icon_size_md, THEME.color_text_on_primary))
-        btn_save.setIcon(save_icon)
-        btn_save.setIconSize(QSize(THEME.icon_size_md, THEME.icon_size_md))
+        btn_save = QPushButton("")
+        apply_icon_button_metrics(btn_save, font_px=THEME.save_button_font_px, object_name="iconPrimary", tooltip="저장")
+        btn_save.setIcon(make_save_icon(THEME.icon_size_md, THEME.color_text_on_primary))
 
-        btn_upload = QPushButton('')
-        apply_icon_button_metrics(btn_upload, font_px=THEME.icon_button_font_px + 2, object_name='iconAction', tooltip='사진 업로드')
+        btn_upload = QPushButton("")
+        apply_icon_button_metrics(btn_upload, font_px=THEME.icon_button_font_px, object_name="iconAction", tooltip="사진 업로드")
         btn_upload.setIcon(make_image_outline_icon(THEME.icon_size_md))
-        btn_upload.setIconSize(QSize(THEME.icon_size_md, THEME.icon_size_md))
 
-        btn_delete_image = QPushButton('')
-        apply_icon_button_metrics(btn_delete_image, font_px=THEME.icon_button_font_px + 2, object_name='iconDanger', tooltip='사진 삭제')
-        delete_icon = parent.style().standardIcon(QStyle.SP_TrashIcon)
-        if delete_icon.isNull():
-            delete_icon = parent.style().standardIcon(QStyle.SP_DialogDiscardButton)
-        btn_delete_image.setIcon(delete_icon)
-        btn_delete_image.setIconSize(QSize(THEME.icon_size_md, THEME.icon_size_md))
-        btn_delete_image.setEnabled(False)
+        btn_delete_image = QPushButton("")
+        apply_icon_button_metrics(btn_delete_image, font_px=THEME.icon_button_font_px, object_name="iconDanger", tooltip="사진 삭제")
+        btn_delete_image.setIcon(parent.style().standardIcon(QStyle.SP_TrashIcon))
 
-        left_controls = QWidget()
-        left_controls_layout = QHBoxLayout(left_controls)
-        left_controls_layout.setContentsMargins(0, 0, 0, 0)
-        left_controls_layout.setSpacing(THEME.top_button_spacing)
-        left_controls_layout.addWidget(btn_back)
-        left_controls_layout.addWidget(btn_reset)
-        left_controls_layout.addWidget(btn_save)
-        left_controls_layout.addStretch(1)
+        header = QWidget()
+        header_layout = QHBoxLayout(header)
+        header_layout.setContentsMargins(0, 0, 0, 0)
+        header_layout.addWidget(btn_back)
+        header_layout.addWidget(btn_reset)
+        header_layout.addWidget(btn_save)
+        header_layout.addStretch()
+        header_layout.addWidget(btn_upload)
+        header_layout.addWidget(btn_delete_image)
 
-        image_controls = QWidget()
-        image_controls_layout = QHBoxLayout(image_controls)
-        image_controls_layout.setContentsMargins(0, 0, 0, 0)
-        image_controls_layout.setSpacing(THEME.top_button_spacing)
-        image_controls_layout.addStretch(1)
-        image_controls_layout.addWidget(btn_upload)
-        image_controls_layout.addWidget(btn_delete_image)
-
-        feedback_label = QLabel('', parent)
+        feedback_label = QLabel("")
         feedback_label.setAlignment(Qt.AlignLeft | Qt.AlignVCenter)
         feedback_label.setMinimumHeight(THEME.feedback_label_height)
-        feedback_label.setMaximumHeight(THEME.feedback_label_height)
-        feedback_label.setStyleSheet(
-            f'QLabel{{background:transparent;border:none;padding:0 {THEME.label_padding_x}px;color:{THEME.color_text};font-weight:700;}}'
-        )
 
         image_preview = ImagePreview()
         image_preview.setMinimumHeight(THEME.image_preview_min_height)
         image_preview.setStyleSheet(image_preview_style())
 
         image_shell = QWidget()
-        image_shell.setObjectName('imageShell')
-        image_shell_layout = QVBoxLayout(image_shell)
-        image_shell_layout.setContentsMargins(
-            THEME.image_shell_margin,
-            THEME.image_shell_margin,
-            THEME.image_shell_margin,
-            THEME.image_shell_margin,
-        )
-        image_shell_layout.setSpacing(0)
-        image_shell_layout.addWidget(image_preview)
-
-        top_row = QWidget()
-        top_layout = QGridLayout(top_row)
-        top_layout.setContentsMargins(0, 0, 0, 0)
-        top_layout.setHorizontalSpacing(THEME.section_gap)
-        top_layout.setVerticalSpacing(THEME.row_spacing)
-        top_layout.setColumnStretch(0, 1)
-        top_layout.setColumnStretch(1, 1)
-        top_layout.addWidget(left_controls, 0, 0, 1, 1)
-        top_layout.addWidget(image_controls, 0, 1, 1, 1)
-        top_layout.addWidget(image_shell, 1, 0, 1, 2)
+        image_layout = QVBoxLayout(image_shell)
+        image_layout.setContentsMargins(THEME.image_shell_margin, THEME.image_shell_margin, THEME.image_shell_margin, THEME.image_shell_margin)
+        image_layout.addWidget(image_preview)
 
         postit_bar = PostItBar()
 
         change_note_postit = ChangeNotePostIt()
-        memo_header = FolderTabHeader('메모', parent)
-        change_note_wrap = SectionContainer(memo_header, change_note_postit, spacing=0, header_alignment=None)
+        change_note_title = SectionTitleBadge("메모", parent)
+        change_note_wrap = SectionContainer(change_note_title, change_note_postit)
 
         content = QWidget()
         content_layout = QGridLayout(content)
         content_layout.setContentsMargins(0, 0, 0, 0)
         content_layout.setHorizontalSpacing(THEME.section_gap)
-        content_layout.setVerticalSpacing(THEME.section_gap)
-        content_layout.addWidget(top_row, 0, 0)
-        content_layout.addWidget(postit_bar, 1, 0)
-        content_layout.addWidget(change_note_wrap, 0, 1, 2, 1)
-        content_layout.setColumnStretch(0, 3)
-        content_layout.setColumnStretch(1, 1)
-        content_layout.setRowStretch(0, 3)
-        content_layout.setRowStretch(1, 1)
+        content_layout.setVerticalSpacing(THEME.row_spacing)
 
-        page_layout.addWidget(content, 1)
-        page_layout.addWidget(feedback_label, 0)
+        right_stack = QWidget()
+        right_layout = QVBoxLayout(right_stack)
+        right_layout.setContentsMargins(0, 0, 0, 0)
+        right_layout.setSpacing(THEME.section_gap)
+
+        right_layout.addWidget(postit_bar)
+        right_layout.addWidget(change_note_wrap)
+
+        content_layout.addWidget(image_shell, 0, 0)
+        content_layout.addWidget(right_stack, 0, 1)
+
+        content_layout.setColumnStretch(0, 2)
+        content_layout.setColumnStretch(1, 1)
+
+        page_layout.addWidget(header)
+        page_layout.addWidget(content)
+        page_layout.addWidget(feedback_label)
 
         return WorkOrderPageRefs(
             page=page,
