1955829a6b63e9655566251987ad9d55ea573690
ChoiWonJae850610 <choiwonjae@kakao.com>
2026-03-15 22:46:23 +0900

    리팩토링
    리팩토링

--- ui/postit/stack.py
+++ ui/postit/stack.py
@@ -1,7 +1,4 @@
-"""Public entry point for post-it stack widgets."""
+from ui.postit.partner_section import PostItBar
+from ui.postit.stack_core import PostItStack
 
-from ui.postit.partner_bar import PostItBar
-from ui.postit.partner_tabs import PartnerTabbedPostIt
-from ui.postit.stack_impl import PostItStack
-
-__all__ = ["PostItBar", "PostItStack", "PartnerTabbedPostIt"]
+__all__ = ["PostItBar", "PostItStack"]
